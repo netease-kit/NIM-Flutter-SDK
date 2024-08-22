8cd9721f6362df2bd370814bcf90800ddce25739
houchengdong <houchengdong@corp.netease.com>
2024-08-22 16:44:01 +0800

    feat:v2 10.3.0 release

--- nim_core_web/web/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import logDebug from 'yunxin-log-debug'
-import packageJson from '../package.json'
-
-export const logger = logDebug({
-  level: 'debug',
-  version: packageJson.version,
-  appName: packageJson.name,
-})
